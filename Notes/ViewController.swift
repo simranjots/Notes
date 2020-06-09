3d38d182aacbe593c123fe28bb4871315270f4b5
richa <richa1998.patel@gmail.com>
2020-06-08 22:51:39 -0400

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Richa Patel on 2020-06-08.
+//  Copyright © 2020 Simranjot singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
